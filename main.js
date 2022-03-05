9b523f3a600b002536158aaf728eee2e8a9c2947
cryptogay <84311767+cryptogay@users.noreply.github.com>
2022-03-05 13:43:24 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log("test");
+
+document.getElementById("menu").addEventListener("click", openMenu)
+function openMenu() {
+    document.getElementById("dropdown-840px").classList.toggle("active")
+}
